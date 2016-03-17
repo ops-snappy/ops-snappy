5ae20d1db7acc230ea1d53edf5bd65707b51ed9b
Loïc Minier <lool@dooz.org>
2016-03-17 01:38:22 +0100

    Add x-autotools plugin; runs a precmd before build

---
+++ parts/plugins/x_autotools.py
@@ -0,0 +1,30 @@
+import snapcraft.plugins.autotools
+import snapcraft
+
+
+class XAutotoolsPlugin(snapcraft.plugins.autotools.AutotoolsPlugin):
+    @classmethod
+    def schema(cls):
+        schema = super().schema()
+        schema['properties']['precmd'] = {
+            'type': 'array',
+            'minitems': 1,
+            'items': {
+                'type': 'string',
+            },
+            'default': [],
+        }
+        return schema
+
+    def build(self):
+        baseplugin = snapcraft.BasePlugin
+        baseplugin.build(self)
+
+        if self.options.precmd:
+            self.run(self.options.precmd)
+
+        save = baseplugin.build
+        baseplugin.build = lambda self: None
+        super().build()
+        baseplugin.build = save
+
