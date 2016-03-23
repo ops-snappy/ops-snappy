e81d9d093273e16c32ed790fd126e9def46fd4a3
Mike Berger <mabnhdev@gmail.com>
2016-03-23 15:01:57 -0400

    Start/Stop Openswitch properly.

--- src/ops-init/start-openswitch.sh
+++ src/ops-init/start-openswitch.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 # Required directories
-/usr/bin/test -d $SNAP_DATA/var/run/openvswitch || mkdir -p $SNAP_DATA/var/run/openvswitch
-/usr/bin/test -d $SNAP_DATA/var/local/openvswitch || mkdir -p $SNAP_DATA/var/local/openvswitch
+DBDIR=$SNAP_DATA/var/run/openvswitch
+VTEPDBDIR=$SNAP_DATA/var/local/openvswitch
+PIDDIR=$DBDIR
+CTLDIR=$PIDDIR
+BINDIR=$SNAP/usr/bin
+SBINDIR=$SNAP/usr/sbin
+SCHEMADIR=$SNAP/usr/share/openvswitch
 
-# Alphanetworks fan control
+for i in $DBDIR $VTEPDBDIR $PIDDIR $CTLDIR ; do
+    /usr/bin/test -d $i || mkdir -p $i
+done
+
+# Alphanetworks software fan control
 if [ -d /sys/class/gpio/gpiochip452 ] ; then
     if [ ! -d /sys/class/gpio/gpio470 ] ; then
         echo '470' | tee --append /sys/class/gpio/export > /dev/null
@@ -13,13 +22,19 @@ if [ -d /sys/class/gpio/gpiochip452 ] ; then
     echo '0' | tee --append /sys/class/gpio/gpio470/value > /dev/null
 fi
 
-# Init
-$SNAP/usr/sbin/ops-init
+# Create the network namespaces
+$SBINDIR/ops-init
+
+# Create the databases if they don't exist.
+/usr/bin/test -f $DBDIR/ovsdb.db || $BINDIR/ovsdb-tool create $DBDIR/ovsdb.db $SCHEMADIR/vswitch.ovsschema
+/usr/bin/test -f $VTEPDBDIR/vtep.db || $BINDIR/ovsdb-tool create $VTEPDBDIR/vtep.db $SCHEMADIR/vtep.ovsschema
+/usr/bin/test -f $DBDIR/dhcp_leases.db || $BINDIR/ovsdb-tool create $DBDIR/dhcp_leases.db $SCHEMADIR/dhcp_leases.ovsschema
+/usr/bin/test -f $DBDIR/config.db || $BINDIR/ovsdb-tool create $DBDIR/config.db $SCHEMADIR/configdb.ovsschema
+/usr/bin/test -f $DBDIR/ovsdb.db || $BINDIR/ovsdb-tool create $DBDIR/ovsdb.db $SCHEMADIR/vswitch.ovsschema
 
 # OVSDB Server
-/usr/bin/test -f $SNAP_DATA/var/run/openvswitch/ovsdb.db || $SNAP/usr/bin/ovsdb-tool create $SNAP_DATA/var/run/opsenvswitch/ovsdb.db $SNAP/usr/share/openvswitch/vswitch.ovsschema
-/usr/bin/test -f /var/local/openvswitch/vtep.db || $SNAP/usr/bin/ovsdb-tool create $SNAP_DATA/var/local/openvswitch/vtep.db $SNAP/usr/share/openvswitch/vtep.ovsschema
-/usr/bin/test -f $SNAP_DATA/var/local/opsenvswitch/dhcp_leases.db || $SNAP/usr/bin/ovsdb-tool create $SNAP_DATA/var/local/opsenvswitch/dhcp_leases.db $SNAP/usr/share/openvswitch/dhcp_leases.ovsschema
-/usr/bin/test -f $SNAP_DATA/var/local/opsenvswitch/config.db || $SNAP/usr/bin/ovsdb-tool create $SNAP_DATA/var/local/opsenvswitch/configdb.db $SNAP/usr/share/openvswitch/configdb.ovsschema
-/usr/bin/test -f $SNAP_DATA/var/run/opsenvswitch/ovsdb.db || $SNAP/usr/bin/ovsdb-tool create $SNAP_DATA/var/run/opsenvswitch/ovsdb.db $SNAP/usr/share/openvswitch/vswitch.ovsschema
-$SNAP/usr/sbin/ovsdb-server --remote=punix:$SNAP_DATA/var/run/openvswitch/db.sock --detach --no-chdir --pidfile -vSYSLOG:INFO $SNAP_DATA/var/local/opsenvswitch/ovsdb.db $SNAP_DATA/var/local/opsenvswitch/config.db
+# TODO - By default, the unix control socket is located at
+#        /var/run/openvswitch/<name>.<pid>.ctl.  Can't dynamically
+#        assign the assign the pid if we are specifying a non-default
+#        location for the pid.
+$SBINDIR/ovsdb-server --remote=punix:$DBDIR/db.sock --detach --no-chdir --pidfile=$PIDDIR/ovsdb-server.pid --unixctl=$CTLDIR/ovsdb-server.ctl -vSYSLOG:INFO $DBDIR/ovsdb.db $DBDIR/config.db $DBDIR/dhcp_leases.db
